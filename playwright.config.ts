d65af3927d6cac9eb74d769ca71bea388fa208e9
shubnit12 <84227718+shubnit12@users.noreply.github.com>
2026-09-12 23:37:33 +0530

    Increase timeout and configure screenshot options

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,12 +24,15 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  timeout: 30_000_000,
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    actionTimeout: 60_000,
   },
 
   /* Configure projects for major browsers */
